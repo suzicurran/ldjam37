548f7f661854348d9c59c59662e048d53aca7be8
suzicurran <suzicurran@gmail.com>
2016-12-10 14:01:27 -0500

    add walls!

--- game.rb
+++ game.rb
@@ -6,19 +6,19 @@ Hasu.load './lib/Tile.rb'
 
 class Game < Hasu::Window
 
-  TILEDIMENSIONS = 32
+  TILEDIMENSIONS = 32 # in px
+  TILES_PER_SIDE = 25
 
-  def initialize(tiles_per_side = 25)
-    @tiles_per_side = tiles_per_side
-    super(@tiles_per_side * TILEDIMENSIONS, @tiles_per_side * TILEDIMENSIONS)
+  def initialize
+    super(TILES_PER_SIDE * TILEDIMENSIONS, TILES_PER_SIDE * TILEDIMENSIONS)
   end
 
   def reset
     # @tile = Tile.new(0,0)
     self.caption = 'Capture the other base!'
     @game_tiles = []
-    @tiles_per_side.times do |index_for_column|
-      @tiles_per_side.times do |index_for_row|
+    TILES_PER_SIDE.times do |index_for_column|
+      TILES_PER_SIDE.times do |index_for_row|
         this_tile = Tile.new(index_for_column, index_for_row)
         @game_tiles << this_tile
       end
--- lib/Tile.rb
+++ lib/Tile.rb
@@ -1,15 +1,31 @@
 class Tile
+
+  WALL = 0
+  GREY = 1
+  RED = 2
+  BLUE = 3
+
   def initialize(column_index, row_index)
     @column_index = column_index  # x value w/ index starting at 0
     @row_index = row_index # y value w/ index starting at 0
     path_to_tile_image = './Images/Tiles.png'
     @board_sprites = Gosu::Image.load_tiles(path_to_tile_image, Game::TILEDIMENSIONS, Game::TILEDIMENSIONS)
+    @tile_color = GREY
+    if is_a_wall?
+      @tile_color = WALL
+    end
+  end
+
+  def is_a_wall?
+    wall_locations = [0, (Game::TILES_PER_SIDE - 1)]
+    wall_locations.include?(@column_index) || wall_locations.include?(@row_index)
   end
 
   def change_color(target_color)
+    @tile_color = target_color
   end
 
   def draw
-    @board_sprites[0].draw(@column_index*Game::TILEDIMENSIONS,@row_index*Game::TILEDIMENSIONS,0)
+    @board_sprites[@tile_color].draw(@column_index*Game::TILEDIMENSIONS,@row_index*Game::TILEDIMENSIONS,0)
   end
 end
