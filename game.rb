aff5f2473706591964b26c840be0e80764b517f2
suzicurran <suzicurran@gmail.com>
2016-12-10 12:46:15 -0500

    added generation of board tiles

--- game.rb
+++ game.rb
@@ -1,15 +1,28 @@
-require 'gosu'
 require 'hasu'
+require 'pry'
+
+Hasu.load './lib/Player.rb'
+Hasu.load './lib/Tile.rb'
 
 class Game < Hasu::Window
-  def initialize(size = 50)
-    @size = size
-    @tilesize = 32
-    super(@size * @tilesize, @size * @tilesize)
+
+  TILEDIMENSIONS = 32
+
+  def initialize(tiles_per_side = 50)
+    @tiles_per_side = tiles_per_side
+    super(@tiles_per_side * TILEDIMENSIONS, @tiles_per_side * TILEDIMENSIONS)
   end
 
   def reset
     self.caption = 'Capture the other base!'
+    @game_tiles = []
+    @tiles_per_side.times do |index_for_column|
+      @tiles_per_side.times do |index_for_row|
+        this_tile = Tile.new(index_for_column, index_for_row)
+        @game_tiles << this_tile
+      end
+    end
+    binding.pry
   end
 
   def update
